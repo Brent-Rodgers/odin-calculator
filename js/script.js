6c3b3eaf392a3ab9536d53806230afac0feeae96
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-10-13 18:48:43 -0600

    add slightly better version
    bug: resets total if equal button is pressed multiple times

--- js/script.js
+++ js/script.js
@@ -33,7 +33,6 @@ let second_operand = null
 let operator = null
 
 const populate_display = function(value) {
-    // display_value = value
     display.textContent = value;
 }
 
@@ -42,47 +41,30 @@ const op_buttons = document.querySelectorAll('.operator-button');
 const equals_button = document.querySelectorAll('.equals-button');
 
 num_buttons.forEach(button => button.addEventListener('click', function(){
-    if (!operator) {
-        value += button.dataset.num
-        populate_display(value);
-    }
+    value += button.dataset.num
+    populate_display(value);
 }, false));
 
 op_buttons.forEach(button => button.addEventListener('click', function(){
-operator = button.dataset.num
-console.log(operator)
-if (!first_operand){
-    first_operand = value;
-    value = "";
-} else {
-    second_operand = value;
-    value = operate(
-        op_dict[operator], 
-        Number(first_operand), 
-        Number(second_operand)
-        )
-    first_operand = value
-    populate_display(value);
-    console.log(value)
-    value = ""
-}
-operator = null
+    operator = button.dataset.num
+    console.log(operator)
+    if (!first_operand){
+        first_operand = value;
+        value = "";
+        populate_display(value);
+    }
 }, false));
 
 equals_button.forEach(button => button.addEventListener('click', function(){
-    if (!operator){
-        first_operand = value;
-        value = "";
-    } else {
+    if (operator) {
         second_operand = value;
         value = operate(op_dict[operator], Number(first_operand), Number(second_operand))
-        first_operand = value
         populate_display(value);
         console.log(value)
-        value = ""
+        first_operand = value;
     }
-}))
 
-// if (first_operand && second_operand && operator){
-//     operate(op_dict[operator], Number(first_operand), Number(second_operand))
-// }
+    first_operand = value;
+    value = "";
+    operator = null
+}))
